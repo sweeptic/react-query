bd3cbefc66a2feb4362b87e6d231271180091897
Attila <59293104+sweeptic@users.noreply.github.com>
2021-10-12 20:36:09 +0200

    04 React Query in Larger App_ Setup, Centralization, Custom Hooks

--- spa_project/client/src/components/staff/hooks/useStaff.ts
+++ spa_project/client/src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction, useState } from 'react';
+import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -6,10 +7,10 @@ import { queryKeys } from '../../../react-query/constants';
 import { filterByTreatment } from '../utils';
 
 // for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get('/staff');
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -21,8 +22,9 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
+  const fallback = []
   // TODO: get data from server via useQuery
-  const staff = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff)
 
   return { staff, filter, setFilter };
 }
--- spa_project/client/src/react-query/queryClient.ts
+++ spa_project/client/src/react-query/queryClient.ts
@@ -1,21 +1,27 @@
 import { QueryClient } from 'react-query';
-// import { createStandaloneToast } from '@chakra-ui/react';
-// import { theme } from '../theme';
+import { createStandaloneToast } from '@chakra-ui/react';
+import { theme } from '../theme';
 
-// const toast = createStandaloneToast({ theme });
+const toast = createStandaloneToast({ theme });
 
-// export function queryErrorHandler(error: unknown): void {
+export function queryErrorHandler(error: unknown): void {
 //   // error is type unknown because in js, anything can be an error (e.g. throw(5))
-//   const id = 'react-query-error';
-//   const title =
-//     error instanceof Error
-//       ? // remove the initial 'Error: ' that accompanies many errors
-//         error.toString().replace(/^Error:\s*/, '')
-//       : 'error connecting to server';
+  const id = 'react-query-error';
+  const title =
+    error instanceof Error
+      ? // remove the initial 'Error: ' that accompanies many errors
+        error.toString().replace(/^Error:\s*/, '')
+      : 'error connecting to server';
 
 //   // prevent duplicate toasts
-//   toast.closeAll();
-//   toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
-// }
+  toast.closeAll();
+  toast({ id, title, status: 'error', variant: 'subtle', isClosable: true });
+}
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+   defaultOptions: {
+      queries: {
+         onError: queryErrorHandler,
+      }
+   }
+});
